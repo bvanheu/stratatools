bfa7fe1cb58aabb659138b4cfb68bc6ac8509960
benjamin vanheuverzwijn <bvanheu@gmail.com>
2015-04-16 18:03:44 -0400

    Added new Fortus 380mc and 450mc machine id, thanks to ashanin

--- stratasys/machine.py
+++ stratasys/machine.py
@@ -29,6 +29,7 @@
 type_to_number = {
     "fox": "2C30478BB7DE81E8",
     "fox2": "2C30479BB7DE81E8",
+    "ktype": "6B2A268B5ED3374A",
     "prodigy": "5394D7657CED641D",
     "quantum": "76C454D532E610F7",
     "uprint": "F3A91DBE6B0B2255",
