477bdb98f8a3e8250de80e1c88b4cab85a82e7ab
Nash Kaminski <nashkaminski@comcast.net>
2017-10-28 22:19:13 -0500

    Implement bi-directional DiagnosticPort_Formatter

--- stratasys/formatter.py
+++ stratasys/formatter.py
@@ -1,6 +1,8 @@
 #
 # See the LICENSE file
 #
+import re
+import binascii
 
 class Formatter:
     def __init__(self):
@@ -14,19 +16,27 @@ class Formatter:
 
 class DiagnosticPort_Formatter(Formatter):
     def __init__(self):
-        pass
+        self.rx = re.compile('^[0-9]{6}: ((?:[0-9a-f-A-F]{2} ?)+).*?$',re.MULTILINE)
 
     def from_source(self, data):
-        formatted = ""
-
-        # TODO - implements me
+        formatted = b''
+        idx = 1
+        for match in self.rx.finditer(data):
+            try:
+                #Get a line of data with whitespace removed
+                line = match.group(1).replace(' ', '')
+                formatted += binascii.unhexlify(line)
+                idx=idx+1
+            except IndexError:
+                print("Error on line %s when reading diag port formatted data" % (idx,))
+                raise
 
         return formatted
 
     def to_destination(self, data):
-        formatted = ""
+        formatted = "\""
 
         for b in data:
-            formatted += binascii.hexlify(b) + ", "
+            formatted += binascii.hexlify(b) + " "
 
-        return formatted[0:-2]
+        return formatted[0:-1] + "\""
