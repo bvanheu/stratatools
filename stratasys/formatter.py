8e8c957ed8886f39a26e1f71e594985b8c0e6166
Nash Kaminski <nashkaminski@comcast.net>
2017-10-28 22:23:37 -0500

    Document changes via comments

--- stratasys/formatter.py
+++ stratasys/formatter.py
@@ -18,6 +18,9 @@ class DiagnosticPort_Formatter(Formatter):
     def __init__(self):
         self.rx = re.compile('^[0-9]{6}: ((?:[0-9a-f-A-F]{2} ?)+).*?$',re.MULTILINE)
 
+#Reads a series of newline delimited lines of the format:
+#000096: 00 00 00 00 00 00 00 00 53 54 52 41 54 41 53 59   ........STRATASY
+
     def from_source(self, data):
         formatted = b''
         idx = 1
@@ -33,6 +36,7 @@ class DiagnosticPort_Formatter(Formatter):
 
         return formatted
 
+#Produces a double-quoted, space separated string suitable for providing to the uPrint's 'ew' diagnostic command
     def to_destination(self, data):
         formatted = "\""
 
